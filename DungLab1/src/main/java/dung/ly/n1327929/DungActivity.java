03cee4c7840ae35588d8bc4b041b25b7cb48d38b
dungly7929 <danks76429@gmail.com>
2021-02-04 15:36:57 -0500

    commit All activity class

--- DungLab1/src/main/java/dung/ly/n1327929/DungActivity.java
+++ DungLab1/src/main/java/dung/ly/n1327929/DungActivity.java
@@ -5,13 +5,54 @@ package dung.ly.n1327929;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class DungActivity extends AppCompatActivity {
-
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button bnt = (Button) findViewById(R.id.button2);
+        EditText eText = (EditText) findViewById(R.id.editText);
+        Intent intent = new Intent(DungActivity.this, LyActivity.class);
+        bnt.setOnClickListener(new View.OnClickListener()
+        {
+
+            public void onClick(View view)
+            {
+                String text = eText.getText().toString();
+                intent.putExtra("Key1", text);
+                startActivity(intent);
+
+            }
+        });
+
     }
-}
+
+    public void ShowToast(View view)
+    {
+        Intent intent = null;
+        Toast toast = Toast.makeText(DungActivity.this,"DUNG LY Moving to Site: CBC",Toast.LENGTH_SHORT);
+        toast.setGravity(Gravity.LEFT | Gravity.TOP,20,30);
+        switch (view.getId())
+        {
+            case R.id.button:
+                intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.cbc.ca/"));
+                startActivity(intent);
+                toast.show();
+        }
+    }
+
+
+
+    }
--- DungLab1/src/main/java/dung/ly/n1327929/LyActivity.java
+++ DungLab1/src/main/java/dung/ly/n1327929/LyActivity.java
@@ -5,7 +5,11 @@ package dung.ly.n1327929;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class LyActivity extends AppCompatActivity {
 
@@ -13,5 +17,18 @@ public class LyActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ly);
+
+    }
+    @Override
+    protected  void onStart()
+    {
+        super.onStart();
+        String value = "";
+        TextView tv =  (TextView) findViewById(R.id.textView2);
+        Snackbar snackbar = Snackbar.make(findViewById(R.id.secondactivity), "DUNG LY", Snackbar.LENGTH_LONG);
+        Intent intent = getIntent();
+        value = intent.getStringExtra("Key1");
+        snackbar.show();
+        tv.setText(value);
     }
 }
